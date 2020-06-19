d9ed6d44574fcfe6bab95422a314af3fb2870ab3
securethebags <anthonyjim96@gmail.com>
2020-06-19 00:16:00 -0400

    fixed view height in pokemon view

--- src/Search.js
+++ src/Search.js
@@ -1,62 +1,55 @@
-import React, { useState, useEffect } from 'react';
-import PokemonListItem from './PokemonListItem'
+import React, { useState, useEffect } from "react";
+import PokemonListItem from "./PokemonListItem";
 
-export default function Search({pokemon, allPokemon}) {
-  
-    
- const [searchResults, setSearchResults] = useState([]);
- const [searchParameters, setSearchParameters] = useState("")
- const [searchTerm, setSearchTerm] = useState("");
+export default function Search({ pokemon, allPokemon }) {
+  const [searchResults, setSearchResults] = useState([]);
+  const [searchParameters, setSearchParameters] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
 
- useEffect(() => {
-  (searchTerm === "") ?
-  setSearchResults([]) :
-  (searchParameters === 'allPokemon') ? 
-    setSearchResults(
-    allPokemon.filter(pokemon => pokemon.toLowerCase().includes(searchTerm))
-      .map(p => p))
-    : 
-    setSearchResults(
-    pokemon.filter(pokemon => pokemon.toLowerCase().includes(searchTerm))
-        .map(p => p))
+  useEffect(() => {
+    searchTerm === ""
+      ? setSearchResults([])
+      : searchParameters === "allPokemon"
+      ? setSearchResults(
+          allPokemon
+            .filter((pokemon) => pokemon.toLowerCase().includes(searchTerm))
+            .map((p) => p)
+        )
+      : setSearchResults(
+          pokemon
+            .filter((pokemon) => pokemon.toLowerCase().includes(searchTerm))
+            .map((p) => p)
+        );
   }, [searchTerm, pokemon]);
 
-const handleTextChange = event => setSearchTerm(event.target.value)
-const handleSelectChange = event => setSearchParameters(event.target.value)
+  const handleTextChange = (event) => setSearchTerm(event.target.value);
+  const handleSelectChange = (event) => setSearchParameters(event.target.value);
 
   return (
     <>
-    <div className = 'searchForm'>
-    <select className = 'select' onChange={handleSelectChange}>
-    <option
-        name="Search current page"
-        value="currentPokemon"
-         >
-        Search Current Page
-      </option>
-      <option
-        name="Search all Pokemon"
-        value="allPokemon"
-      >
-      Search All Pokemon
-      </option>
-      </select>
+      <div className="searchForm">
+        <select className="select" onChange={handleSelectChange}>
+          <option name="Search current page" value="currentPokemon">
+            Search Current Page
+          </option>
+          <option name="Search all Pokemon" value="allPokemon">
+            Search All Pokemon
+          </option>
+        </select>
 
         <div>
-            <input 
-            className = 'search'
-            type = "text"
-            placeholder = "Search"
-            value = {searchTerm}
-            onChange = {handleTextChange}
-            />
-            {searchResults.map(item => (
-            <PokemonListItem pokemon={item}/>
-            ))
-            }
-       
+          <input
+            className="search"
+            type="text"
+            placeholder="Search"
+            value={searchTerm}
+            onChange={handleTextChange}
+          />
+          {searchResults.map((item) => (
+            <PokemonListItem pokemon={item} />
+          ))}
         </div>
-        </div>
+      </div>
     </>
-    )
+  );
 }
