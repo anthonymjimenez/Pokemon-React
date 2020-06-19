11c341af1d6032b438a99cd2350ce2be903f8258
securethebags <anthonyjim96@gmail.com>
2020-06-18 23:34:15 -0400

    continued to work on styling, added type based color to pokemon view

--- src/PokemonView.js
+++ src/PokemonView.js
@@ -1,50 +1,59 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 const CharacterView = ({ match }) => {
   const [character, setCharacter] = useState({});
-  const [characterID, setID] = useState(null)
+  const [characterID, setID] = useState(null);
   useEffect(() => {
     fetch(`https://pokeapi.co/api/v2/pokemon/${match.params.id}`)
-      .then(response => response.json())
-      .then(response => {setCharacter(response)
-                         setID(response.id)
-                        })
-    
+      .then((response) => response.json())
+      .then((response) => {
+        setCharacter(response);
+        setID(response.id);
+      });
   }, [match.params.id]);
 
-const arrayCheck = (arg) => { return (Array.isArray(arg)) ? (arg) : [] }
+  const arrayCheck = (arg) => {
+    return Array.isArray(arg) ? arg : [];
+  };
 
   return (
     <>
-    <section className="pokemonView">
-      <h2>{character.name}</h2>
-      <ul className="pokemonDetails">
-    <li>
-        <strong>Type: </strong> 
-        {arrayCheck(character.types).map(({type}) => <p>{type.name}</p>)} 
-    </li>
-    <li>
-          <strong>Height</strong>: {character.height}
-    </li>
-    <li>
-          <strong>Weight</strong>: {character.weight}
-    </li> 
-    <li>
-        <strong>Base Stats: </strong>
-        {arrayCheck(character.stats).map(({stat, base_stat, effort}) => 
-      <>
-        <p>Stat: {stat.name}</p>
-        <p>Base Stat: {base_stat}</p>
-        <p>Effort: {effort}</p>
-      </>
-        )}
-    </li>
-    </ul>
-    </section>
-    <img className ='pokemonImage' src={`https://pokeres.bastionbot.org/images/pokemon/${characterID}.png`} alt="Logo" />
-  </>
-    );
-  };
-
+      <section className="pokemonView">
+        <h2>{character.name}</h2>
+        <ul className="pokemonDetails">
+          <li>
+            <strong>Type: </strong>
+            {arrayCheck(character.types).map(({ type }) => (
+              <p className={type.name}>{type.name}</p>
+            ))}
+          </li>
+          <li>
+            <strong>Height</strong>: {character.height}
+          </li>
+          <li>
+            <strong>Weight</strong>: {character.weight}
+          </li>
+          <li>
+            <strong>Base Stats: </strong> <br />
+            {arrayCheck(character.stats).map(({ stat, base_stat, effort }) => (
+              <>
+                <strong>
+                  {stat.name[0].toUpperCase() + stat.name.slice(1)}
+                </strong>
+                <li>Base Stat: {base_stat}</li>
+                <li>Effort: {effort}</li>
+              </>
+            ))}
+          </li>
+        </ul>
+      </section>
+      <img
+        className="pokemonImage"
+        src={`https://pokeres.bastionbot.org/images/pokemon/${characterID}.png`}
+        alt="Logo"
+      />
+    </>
+  );
+};
 
 export default CharacterView;
